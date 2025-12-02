f2fc10ff4e65aa051b586170372da65c4c134a4a
Maja Gacesa <maja.gacesa@gmail.com>
2025-12-02 09:02:38 +0100

    add ktlint to project and format files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.1.0"
+    id("org.jlleitschuh.gradle.ktlint") version "11.6.1"
 }
 
 sourceSets {
@@ -11,5 +12,15 @@ sourceSets {
 tasks {
     wrapper {
         gradleVersion = "8.11.1"
+    }
+}
+
+ktlint {
+    verbose.set(true)
+    outputToConsole.set(true)
+    coloredOutput.set(true)
+    reporters {
+        reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.CHECKSTYLE)
+        reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.HTML)
     }
 }
--- src/Day01.kt
+++ src/Day01.kt
@@ -10,41 +10,34 @@ fun main() {
                     .slice(0 until 1) to text.slice(1 until text.length).toInt()
             }
 
-    
     val zeroes = instructions
         .fold(initial = 50 to 0) { (position, count), (direction, distance) ->
-            val increment = when (direction) {"L" -> -distance "R" -> distance else -> 0}
+            val increment = when (direction) { "L" -> -distance "R" -> distance else -> 0 }
             (increment + position).mod(100) to if (position == 0) count + 1 else count
         }
-    
+
     val zeroPasses = instructions
         .fold(initial = 50 to 0) { (position, count), (direction, distance) ->
-            val increment = when (direction) {"L" -> -distance "R" -> distance else -> 0}
+            val increment = when (direction) { "L" -> -distance "R" -> distance else -> 0 }
             val previous = position
             val next = (increment + position).mod(100)
 
             val fullLoops = abs(increment.div(100))
-            val throughZero = when(direction) {
+            val throughZero = when (direction) {
                 "L" -> if (previous != 0 && previous <= next) 1 else 0
                 "R" -> if (next != 0 && previous >= next) 1 else 0
                 else -> 0
             }
-            
+
             next to count + fullLoops + throughZero
         }
-           
-    
 
-    
     val part1 = zeroes.second
     val part2 = zeroPasses.second + part1
-    
-    
+
     println("Part 1 solution is: $part1")
     println("Part 2 solution is: $part2")
 
     assert(part1 == 1168)
     assert(part2 == 7199)
-
 }
-    
