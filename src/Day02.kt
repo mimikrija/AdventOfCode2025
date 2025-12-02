87451e3ae7adc86e67b8c4a3fad312422a22fa81
Maja Gacesa <maja.gacesa@gmail.com>
2025-12-02 10:49:49 +0100

    [day02] I just learned about `chunked` (but this is slower)
    I am aware that there's code repetition here, but I kinda prefer it this
    way

--- src/Day02.kt
+++ src/Day02.kt
@@ -12,8 +12,7 @@ fun main() {
             val str = number.toString()
             val size = str.length
             if (size < 2 || size % 2 == 1) return@sumOf 0
-            val checks = str.slice(0 until size.div(2)) to str.slice(size.div(2) until size)
-            if (checks.first == checks.second) number else 0
+            if (str.chunked(size/2).toSet().size == 1) number else 0
         }
     }
 
@@ -22,12 +21,8 @@ fun main() {
             val str = number.toString()
             val size = str.length
             if ((1..size/2)
-                    .any { windowSize -> 
-                        if (size % windowSize != 0) return@any false
-                        str
-                            .windowed(size = windowSize, step = windowSize)
-                            .zipWithNext()
-                            .all { (left, right) -> left == right } 
+                    .any { chunkSize ->
+                        str.chunked(chunkSize).toSet().size == 1
                     }
                 ) number else 0 
         } 
@@ -39,3 +34,4 @@ fun main() {
     assert(part1 == 53420042388)
     assert(part2 == 69553832684)
 }
+
