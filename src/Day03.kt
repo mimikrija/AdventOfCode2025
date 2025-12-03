a286528a902464e0f2da910996ba7d7eee943c65
Maja Gacesa <maja.gacesa@gmail.com>
2025-12-03 10:02:00 +0100

    [day03] pt2 too slow

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,13 +1,15 @@
+import kotlin.math.max
+
 fun main() {
     val batteries = readInput("Day03")
 
 
-    val part1 = batteries.sumOf { 
-        val x = maxJoltage(it)
-    x}
-    
-  println("Part 1 solution is: $part1")
-//    println("Part 2 solution is: $part2")
+    val part1 = batteries.sumOf { maxJoltage(it) }
+
+    val part2 = batteries.sumOf { battery -> bla("", battery).toLong() }
+
+    println("Part 1 solution is: $part1")
+    println("Part 2 solution is: $part2")
 //
 //    assert(part1 == 53420042388)
 //    assert(part2 == 69553832684)
@@ -27,4 +29,16 @@ private fun maxJoltage(battery: String): Int {
             .toString()
         (left + right).toInt()
     }
+}
+
+
+private fun bla(final: String = "", rest: String, currentMax: Long = 0): String {
+    val size = rest.length
+    val stop = 12 - final.length - 1
+    if (final.length == 12) return final
+    return (0 until size - stop).map { pos ->
+        val left = rest[pos]
+        bla(final + left, rest.slice(pos + 1 until size))
+    }.maxOf { it }
+
 }
