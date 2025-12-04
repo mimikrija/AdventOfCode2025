2712d4aebb9b4c85f7d8fb1333e505f91c9ee7a8
Maja Gacesa <maja.gacesa@gmail.com>
2025-12-04 06:33:34 +0100

    [day04] second part + clean up of first

--- src/Day04.kt
+++ src/Day04.kt
@@ -3,28 +3,37 @@ fun main() {
         .flatMapIndexed { row, line ->
             line
                 .mapIndexed { column, letter -> Coordinate(column, row) to letter.toString() }
-        }.associate { (position, letter) -> position to letter }
+        }
+        .filter{ it.second == "@"}
+        .map{ it.first }
+        .toSet()
     
-    val gridSize = rollMap.keys.maxOf { it.x } + 1
-
+    val gridSize = rollMap.maxOf { it.x } + 1
 
     
-    val part1 = rollMap.filter { (position, value) ->
-        if (value != "@") return@filter false
-        val neighbours = neighbors(position, gridSize, includeDiagonals = true)
-        neighbours.count { rollMap[it] == "@" } < 4
-    }.size
+    val part1 = accessibleRolls(rollMap, gridSize).size
+    val part2 = rollMap.size - keepRemoving(rollMap, gridSize).size
 
 
 
     println("Part 1 solution is: $part1")
-
-
+    println("Part 1 solution is: $part2")
+    
     assert(part1 == 1493)
+    assert(part1 == 9194)
 
 }
 
+private fun accessibleRolls(rollMap: Set<Coordinate>, gridSize: Int) = rollMap.filter { position ->
+    val neighbours = neighbors(position, gridSize, includeDiagonals = true)
+    neighbours.count { it in rollMap } < 4
+}
 
-
-    
-
+private fun keepRemoving(
+    rollMap: Set<Coordinate>,
+    gridSize: Int
+): Set<Coordinate> {
+    val accessibleRolls = accessibleRolls(rollMap, gridSize = gridSize)
+    if (accessibleRolls.isEmpty()) return rollMap
+    return keepRemoving(rollMap - accessibleRolls, gridSize)
+}
