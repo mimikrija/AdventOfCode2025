0a8e9ff01cd85b2041b7496adb85c7827cd3f4be
Maja Gacesa <maja.gacesa@gmail.com>
2025-12-08 14:47:52 +0100

    [day08] both parts

--- src/Day08.kt
+++ src/Day08.kt
@@ -4,21 +4,16 @@ fun main() {
     val coordinates = readInput("Day08")
         .map { it.split("," ).toCoordinate3D()}
 
-    
     val closestCoordinates = coordinates
-        .flatMapIndexed { i, first -> 
+        .flatMapIndexed { i, first ->
             coordinates.slice(i + 1 until coordinates.size).map { second ->
-                    listOf(first, second).sortedBy { it.x } }}
-            .sortedBy { it.first().euclideanDistanceSquared(it.last()) }
-
+                listOf(first, second).sortedBy { it.x } }}
+        .sortedBy { it.first().euclideanDistanceSquared(it.last()) }
     
     val part1 = connect(closestCoordinates.take(1000))
-    
-
-   println("Part 1 solution is: $part1")
-    
     val part2 = connect(closestCoordinates, part2 = true)
     
+    println("Part 1 solution is: $part1")
     println("Part 2 solution is: $part2") 
 
     assert(part1 == 42840)
@@ -27,28 +22,36 @@ fun main() {
 
 
 private fun connect(
-    coordinatePairs: List<List<Coordinate3D>>,
+    closestCoordinates: List<List<Coordinate3D>>,
     part2: Boolean = false,
 ): Int {
-    val allCoordinates = coordinatePairs.flatMap { it }.toSet()
-        .map{setOf(it).toMutableSet() }.toMutableList()
     
-    coordinatePairs
-        .forEach { (first, second) -> 
-            val firstSet = allCoordinates.first { it.contains(first) }
-            val secondSet = allCoordinates.first { it.contains(second) }
+    val connections = closestCoordinates
+        .flatten()
+        .toSet()
+        .map{ mutableSetOf(it)}
+        .toMutableList()
+    
+    closestCoordinates
+        .forEach { (first, second) ->
+            val firstSet = connections.first { it.contains(first) }
+            val secondSet = connections.first { it.contains(second) }
             if (firstSet != secondSet) {
-                allCoordinates.remove(firstSet)
-                allCoordinates.remove(secondSet)
+                connections.remove(secondSet)
                 firstSet.addAll(secondSet)
-                allCoordinates.add(firstSet)}
-            val setSizes = allCoordinates.map{it.size}.toSet()
-            if (setSizes.size == 1 && part2) return first.x * second.x
+                }
+            // part 2 solution
+            if (connections.size == 1 && part2) return first.x * second.x
         }
-
-    return allCoordinates.map{it.size}.sortedDescending().take(3).fold(1) { acc, i -> acc * i }
-
+    // part 1 solution
+    return connections
+        .map{it.size}
+        .sortedDescending()
+        .take(3)
+        .fold(1) { acc, i -> acc * i }
 }
 
 private fun Coordinate3D.euclideanDistanceSquared(other: Coordinate3D) =
-    (x - other.x).toDouble().pow(2) + (y - other.y).toDouble().pow(2) + (z - other.z).toDouble().pow(2)
+    (x - other.x).toDouble().pow(2) +
+            (y - other.y).toDouble().pow(2) + 
+            (z - other.z).toDouble().pow(2)
